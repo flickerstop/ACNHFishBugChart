3f213f48a2d352a1e7859c5cde4788de94ef7efc
Nuuka <mohamedashour12@gmail.com>
2020-04-22 21:21:12 -0400

    Made fixes and additions to data, added launch config for nodejs applet

--- applets/buildJson/run.js
+++ applets/buildJson/run.js
@@ -1,7 +1,7 @@
 let fs = require("fs-extra");
 
 
-let bugsCSV = fs.readFileSync("./bugs.csv").toString();
+let bugsCSV = fs.readFileSync("./applets/buildJson/bugs.csv").toString();
 
 // split rows
 let bugRows = bugsCSV.split("\n");
@@ -13,26 +13,30 @@ let bugsJson = []
 for(let bugRow of bugRows){
     bugRow = bugRow.split("|");
     let bugData = {
-        id: bugRow[0],
+        id: parseInt(bugRow[0]),
         name: bugRow[1],
         found: bugRow[2],
-        price: bugRow[3],
-        timeString: bugRow[4],
-        time:bugRow[5],
-        dateStringN:bugRow[6],
-        dateStringS:bugRow[7],
-        dateN:bugRow[8],
-        dateS:bugRow[9]
+        foundCategory: parseInt(bugRow[3]),
+        weather: parseInt(bugRow[4]),
+        price: bugRow[5],
+        rarity: bugRow[6],
+        rarityCategory: parseInt(bugRow[7]),
+        timeString: bugRow[8],
+        time:bugRow[9].replace("[","").replace("]","").split(",").map(x=>+x),
+        dateStringN:bugRow[10],
+        dateStringS:bugRow[11],
+        dateN:bugRow[12].replace("[","").replace("]","").split(",").map(x=>+x),
+        dateS:bugRow[13].replace("[","").replace("]","").split(",").map(x=>+x)
     }
     bugsJson.push(bugData);
 }
 
-//fs.writeJSONSync("./bugs.json",bugsJson, {spaces: "\t"});
+fs.writeJSONSync("./applets/buildJson/bugs.json",bugsJson, {spaces: "\t"});
 
 //////////////////////////////////
 // fish 
 
-let fishCSV = fs.readFileSync("./fish.csv").toString();
+let fishCSV = fs.readFileSync("./applets/buildJson/fish.csv").toString();
 
 // split rows
 let fishRows = fishCSV.split("\n");
@@ -44,19 +48,23 @@ let fishJson = []
 for(let fishRow of fishRows){
     fishRow = fishRow.split("|");
     let fishData = {
-        id: fishRow[0],
+        id: parseInt(fishRow[0]),
         name: fishRow[1],
         found: fishRow[2],
-        size: fishRow[3],
-        price: fishRow[4],
-        timeString: fishRow[5],
-        time:fishRow[6],
-        dateStringN:fishRow[7],
-        dateStringS:fishRow[8],
-        dateN:fishRow[9],
-        dateS:fishRow[10]
+        foundCategory: parseInt(fishRow[3]),
+        size: fishRow[4],
+        sizeCategory: parseInt(fishRow[5]),
+        price: fishRow[6],
+        rarity: fishRow[7],
+        rarityCategory: parseInt(fishRow[8]),
+        timeString: fishRow[9],
+        time:fishRow[10].replace("[","").replace("]","").split(",").map(x=>+x),
+        dateStringN:fishRow[11],
+        dateStringS:fishRow[12],
+        dateN:fishRow[13].replace("[","").replace("]","").split(",").map(x=>+x),
+        dateS:fishRow[14].replace("[","").replace("]","").split(",").map(x=>+x)
     }
     fishJson.push(fishData);
 }
 
-//fs.writeJSONSync("./fish.json",fishJson, {spaces: "\t"});
+fs.writeJSONSync("./applets/buildJson/fish.json",fishJson, {spaces: "\t"});
